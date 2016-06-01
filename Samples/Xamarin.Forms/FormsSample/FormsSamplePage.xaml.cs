ce4247dbf11875a64eab48cfa2cce9870d354593
Mike James <mike@xamarin.com>
2016-06-01 21:32:05 +0100

    Added Forms projects

---
+++ Samples/Xamarin.Forms/FormsSample/FormsSamplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace FormsSample
+{
+    public partial class FormsSamplePage : ContentPage
+    {
+        public FormsSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
